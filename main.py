df0aaee1096b0a4be2ce7b324fcb2d880a82e522
student1 <student1@class.local>
2025-09-12 09:17:33 +0900

    Добавил вывод текущей даты

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('my first project')
+import requests
+import datetime
+response = requests.get('https://google.com')
+print("status of code answer:", response.status_code)
+print("Length of answer:",len(response.text))
+print("Текущая дата:", datetime.datetime.now())
